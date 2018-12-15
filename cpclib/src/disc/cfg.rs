8bcea95e82d6fb35efac374abfa0667601e4bbdc
rgiot <giot.romain@gmail.com>
2018-12-15 19:02:04 +0100

    Farse Side A

--- cpclib/src/disc/cfg.rs
+++ cpclib/src/disc/cfg.rs
@@ -2,21 +2,48 @@
 
 
 use nom;
-use nom::{hex_u32, space0, space1, eol};
+use nom::{hex_u32, space0, space1, eol };
 use nom::types::CompleteStr;
 use std::str::FromStr;
 
 #[derive(Debug)]
 pub struct DiscConfig {
 	nb_track: u16,
-	nb_side: u16
+	nb_side: u16,
+	track_groups: Vec<TrackGroup>
 }
+
+
+#[derive(Debug)]
+pub enum Side {
+	SideA,
+	SideB
+}
+
+
+#[derive(Debug)]
+pub struct TrackGroup {
+	tracks: Vec<u16>,
+	side: Side,
+	sector_size: u16,
+	gap3: u16,
+	sector_id: Vec<u16>,
+	sector_id_head: Vec<u16>,
+}
+
 
 
 named!(value<CompleteStr, u16>,
 alt!(hex|dec)
 );
 
+
+named!(list_of_values<CompleteStr, Vec<u16>>,
+	separated_list!(
+		tag!(","),
+		value
+	)
+);
 
 
 
@@ -32,7 +59,6 @@ fn is_hex_digit(c: char) -> bool {
 fn is_dec_digit(c: char) -> bool {
   c.is_digit(10)
 }
-
 
 
 named!(hex<CompleteStr, u16>,
@@ -76,6 +102,26 @@ named_args!(value_of_key<'a>(key: &str)<CompleteStr<'a>, u16>,
 );
 
 
+named_args!(list_of_key<'a>(key: &str)<CompleteStr<'a>, Vec<u16>>,
+	do_parse!(
+		space0 >>
+		tag_no_case!(key) >>
+		space0 >>
+		tag!("=") >>
+		space0 >>
+		vals: list_of_values >>
+		space0 >>
+		opt!(eol) >>
+		(
+			vals
+		)
+	)
+);
+
+
+
+
+
 named!(empty_line<CompleteStr, ()>,
  do_parse!(
 	 space0 >>
@@ -87,15 +133,63 @@ named!(empty_line<CompleteStr, ()>,
 );
 
 
+
+named!(track_group_sided<CompleteStr, TrackGroup>,
+do_parse!(
+	side: delimited!(
+		tag_no_case!("[Track-"), 
+		alt!(
+			tag_no_case!("A") => {|_|{Side::SideA}} |
+			tag_no_case!("B") => {|_|{Side::SideB}}
+		),
+	 tag_no_case!(":")
+ ) >>
+ tracks: list_of_values >>
+ tag_no_case!("]") >>
+	many0!(empty_line) >>
+	sector_size: call!(value_of_key, "SectorSize") >>
+		many0!(empty_line) >>
+		gap3: call!(value_of_key, "Gap3") >>
+		many0!(empty_line) >>
+		sector_id: call!(list_of_key, "SectorId") >>
+		sector_id_head: call!(list_of_key, "SectorIdHead") >>
+		empty_line >>
+	(
+		TrackGroup{
+			tracks: tracks,
+			side: side,
+			sector_size,
+			gap3,
+			sector_id,
+			sector_id_head
+		}
+	)
+)
+);
+
+
 named!(pub parse_config<CompleteStr, DiscConfig>,
   do_parse!(
-		opt!(empty_line) >>
+		many0!(empty_line) >>
 	  nb_track: call!(value_of_key, "NbTrack") >>
+		many0!(empty_line) >>
 	  nb_side: call!(value_of_key, "NbSide") >>
+		track_groups: fold_many1!(
+			 preceded!(
+			  many0!(empty_line),
+			  track_group_sided
+		   ),
+			 Vec::new(),
+			 |mut acc: Vec<_>, item|{
+				 acc.push(item);
+				 acc
+			 }
+		 ) >>
 	(
 		DiscConfig {
 			nb_track,
-			nb_side
+			nb_side,
+			track_groups
 		}
 	)
 
