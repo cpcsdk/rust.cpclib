93c04fa0d8560926ca51c12af0458a5bc85e7781
rgiot <giot.romain@gmail.com>
2019-07-14 19:36:49 +0200

    Add a watch option to xfer

--- src/bin/xfer/main.rs
+++ src/bin/xfer/main.rs
@@ -18,8 +18,42 @@ use std::path::Path;
 
 mod interact;
 mod parser;
-
+use hotwatch::{Hotwatch, Event};
 use cpclib as cpc;
+
+use crossbeam_channel::unbounded;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use std::time::Duration;
+
+/// Send and run the file on the CPC.
+/// Snapshot V3 are downgraded to the V2 version
+fn send_and_run_file(xfer: & cpc::xfer::CpcXfer, fname: &str) {
+        let mut done = false;
+        // Snapshot needs to be converted in V2 format and handled differently
+        if let Some(extension) = std::path::Path::new(fname).extension() {
+            let extension = extension.to_str().unwrap().to_ascii_lowercase();
+            if extension == "sna" {
+                let sna = crate::cpc::sna::Snapshot::load(fname);
+                if sna.version_header() == 3 {
+                    eprintln!("Need to downgrade SNA version");
+                    let sna_fname = std::path::Path::new(fname)
+                        .file_name()
+                        .unwrap()
+                        .to_str()
+                        .unwrap();
+                    sna.save(sna_fname, crate::cpc::sna::SnapshotVersion::V2)
+                        .unwrap();
+                    xfer.upload_and_run(sna_fname, None)
+                        .expect("Unable to launch SNA");
+                    done = true;
+                }
+            }
+        }
+        if !done {
+            xfer.upload_and_run(fname, None)
+                .expect("Unable to launch file")
+        };
+}
 
 fn main() -> Result<(), cpc::xfer::XferError> {
     let matches = clap::App::new("CPC xfer to M4")
@@ -42,6 +76,14 @@ fn main() -> Result<(), cpc::xfer::XferError> {
             clap::SubCommand::with_name("-y")
             .about("Upload a file on the M4 in the /tmp folder and launch it. V3 snapshots are automatically downgraded to V2 version")
             .arg(
+                clap::Arg::with_name("WATCH")
+                    .help("Watch the file and resend it on the M4 if modified (so xfer does not end when started with this option).")
+                    .short("w")
+                    .long("watch")
+                    .multiple(false)
+                    .takes_value(false)
+            )
+            .arg(
                 clap::Arg::with_name("fname")
                 .help("Filename to send and execute. Can be an executable (Amsdos header expected) or a snapshot V2")
                 .validator(|fname| {
@@ -53,6 +95,7 @@ fn main() -> Result<(), cpc::xfer::XferError> {
                     }
                 })
                 .required(true)
+                .last(true)
             )
         )
         .subcommand(
@@ -96,31 +139,35 @@ fn main() -> Result<(), cpc::xfer::XferError> {
         xfer.reset_cpc()?;
     } else if let Some(y_opt) = matches.subcommand_matches("-y") {
         let fname = y_opt.value_of("fname").unwrap();
-        let mut done = false;
-        // Snapshot needs to be converted in V2 format and handled differently
-        if let Some(extension) = std::path::Path::new(fname).extension() {
-            let extension = extension.to_str().unwrap().to_ascii_lowercase();
-            if extension == "sna" {
-                let sna = crate::cpc::sna::Snapshot::load(fname);
-                if sna.version_header() == 3 {
-                    eprintln!("Need to downgrade SNA version");
-                    let sna_fname = std::path::Path::new(fname)
-                        .file_name()
-                        .unwrap()
-                        .to_str()
-                        .unwrap();
-                    sna.save(sna_fname, crate::cpc::sna::SnapshotVersion::V2)
-                        .unwrap();
-                    xfer.upload_and_run(sna_fname, None)
-                        .expect("Unable to launch SNA");
-                    done = true;
-                }
+
+        // Simple file sending
+        send_and_run_file(&xfer, fname);
+
+        if y_opt.is_present("WATCH") {
+            // Create a channel to receive the events.
+            let (tx, rx) = unbounded();
+
+            // Automatically select the best implementation for your platform.
+            let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2)).expect("Unable to install watcher");
+
+            // Add a path to be watched. All files and directories at that path and
+            // below will be monitored for changes.
+            watcher.watch(fname, RecursiveMode::NonRecursive).expect("Unable to watch file.");
+
+            loop {
+                match rx.recv() {
+                Ok(Ok(notify::Event{ kind: notify::EventKind::Modify(_),  ..})) |
+                Ok(Ok(notify::Event{ kind: notify::EventKind::Create(_),  ..}))
+                => {
+                    println!("File modified");
+                    send_and_run_file(&xfer, fname);
+                },
+                Err(err) => println!("watch error: {:?}", err),
+                _ => {}
+                };
             }
+
         }
-        if !done {
-            xfer.upload_and_run(fname, None)
-                .expect("Unable to launch file")
-        };
     } else if let Some(x_opt) = matches.subcommand_matches("-x") {
         let fname = x_opt.value_of("fname").unwrap();
         xfer.run(fname)?; /*.expect("Unable to launch file on CPC.");*/
