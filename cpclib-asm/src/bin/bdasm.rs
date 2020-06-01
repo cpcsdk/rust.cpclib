4414e64670ac8554958799c4e956dffc1a5dfa87
rgiot <giot.romain@gmail.com>
2020-06-01 14:59:27 +0200

    Allow to specify blocs of data when disassembling a file

--- cpclib-asm/src/bin/bdasm.rs
+++ cpclib-asm/src/bin/bdasm.rs
@@ -38,6 +38,15 @@ fn main() {
 							.required(false)
 
 					)
+					.arg(
+						Arg::with_name("DATA_BLOC")
+						.help("Relative position that contains data RELATIVE_START-SIZE")
+						.short("d")
+						.long("data")
+						.takes_value(true)
+						.number_of_values(1)
+						.multiple(true)
+					)
 					.get_matches();
 	
 	// Get the bytes to disassemble
@@ -63,20 +72,80 @@ fn main() {
 	
 	// Disassemble
 	eprintln!("0x{:x} bytes to disassemble", input_bytes.len());
-	let listing = {
-		let mut listing = cpclib_asm::disass::disassemble(&input_bytes);
 
-		// add origin if any
-		if let Some(address) = matches.value_of("ORIGIN") {
-			let origin = u16::from_str_radix(address, 16).unwrap();
-			listing.insert(0, org(origin));
+	// Retreive the listing
+	// TODO move that in its own function
+	let mut listing : Listing = if matches.is_present("DATA_BLOC") {
+		// retreive the blocs and parse them
+		let mut blocs = matches
+							.values_of("DATA_BLOC")
+							.unwrap()
+							.map(|bloc|{
+								let split = bloc.split("-").collect::<Vec<_>>();
+								(
+									usize::from_str_radix(split[0], 16).unwrap(), // start in hexa
+									usize::from_str_radix(split[1], 10).unwrap(), // lenght in decimal
+								)
+							})
+							.collect::<Vec<_>>();
+		blocs.sort();
+
+		// make the listing for each of the blocs
+		let mut listings: Vec<Listing> = Vec::new();
+		let mut current_idx = 0;
+		while ! blocs.is_empty() {
+			let &(bloc_idx, bloc_length) = blocs.first().unwrap();
+			if current_idx < bloc_idx {
+				listings.push(
+					cpclib_asm::disass::disassemble(
+						&input_bytes[current_idx..(bloc_idx-current_idx)]
+					)
+				);
+				current_idx = bloc_idx;
+			}
+			else {
+				assert_eq!(current_idx, bloc_idx);
+				listings.push(
+					defb_elements(
+						&input_bytes[current_idx..(current_idx+bloc_length)]).into()
+				);
+				blocs.remove(0);
+				current_idx += bloc_length;
+			}
 		}
-		else {if let Some(origin) = amsdos_load {
-			listing.insert(0, org(origin));
+		if current_idx < input_bytes.len() {
+			listings.push(
+				cpclib_asm::disass::disassemble(
+					&input_bytes[current_idx..]
+				)
+			);
 		}
-		}
-		listing
+
+		// merge the blocs
+		listings.into_iter().fold(
+			Listing::new(),
+			|mut lst, current| {
+				lst.inject_listing(&current);
+				lst
+			}
+		)
+	}
+	else {
+		// no blocs
+		cpclib_asm::disass::disassemble(&input_bytes)
 	};
+
+	// add origin if any
+	if let Some(address) = matches.value_of("ORIGIN") {
+		let origin = u16::from_str_radix(address, 16).unwrap();
+		listing.insert(0, org(origin));
+	}
+	else {
+		if let Some(origin) = amsdos_load {
+			listing.insert(0, org(origin));
+		}
+	}
+	
 
 	println!("{}", listing.to_enhanced_string());
 
