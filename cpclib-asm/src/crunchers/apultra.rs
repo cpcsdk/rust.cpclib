dfc380d6a53b79a8666071a51fa5ffc8e4d3c3b8
rgiot <giot.romain@gmail.com>
2020-08-28 23:28:41 +0200

    [untested] Add support to apultra

---
+++ cpclib-asm/src/crunchers/apultra.rs
@@ -0,0 +1,49 @@
+
+extern {
+	fn APULTRA_crunch(
+		data: * const libc::c_uchar, 
+		len: libc::c_int, 
+		dataout: * mut * mut libc::c_uchar, 
+		lenout: * mut libc::c_int
+	) -> libc::c_int;
+}
+
+/// Compress the givne block using apultra method
+pub fn compress(data: &[u8]) -> Vec<u8> {
+
+	unsafe {
+
+		let len = data.len() as libc::c_int;
+		let data = data.as_ptr();
+
+		let mut dataout : * mut libc::c_uchar = std::ptr::null_mut() ;
+		let mut lenout : libc::c_int = 0;
+
+
+        let res = APULTRA_crunch(
+			data,
+			len,
+			&mut dataout,
+			&mut lenout
+		);
+
+		// copy the crunched C bytes in a rsut struct
+		let crunched = {
+			let mut crunched = Vec::new();
+			crunched.reserve(lenout as usize);
+			for idx in 0..(lenout as isize) {
+				crunched.push(*dataout.offset(idx));
+			}
+			crunched
+		};
+
+		if lenout > 0 {
+			libc::free(dataout as _);
+		}
+
+
+		println!("AP ultra: {} => {}", len, lenout);
+		crunched
+	}
+	
+}
