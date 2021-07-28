ff8d8c5b9c8a315e640e20b18d99284e24225d58
Romain Giot <romain.giot@u-bordeaux.fr>
2021-07-28 23:14:10 +0200

    Ensure multilines directive are properly displaye by the output listing

--- cpclib-asm/src/assembler/listing_output.rs
+++ cpclib-asm/src/assembler/listing_output.rs
@@ -1,6 +1,8 @@
 use std::{fmt::Debug, io::Write};
 
 use itertools::Itertools;
+use lazy_static::__Deref;
+use nom::ExtendInto;
 
 
 use crate::preamble::LocatedToken;
@@ -12,12 +14,14 @@ pub struct ListingOutput {
 	/// The listing is produced line by line and not token per token
 	writer: Box<dyn Write>,
 
+	// the complete source
+	current_source: Option<(*const u8, usize)>,
 	/// The line that will be printed when all the tokens will be injected
 	current_line: Option<(*const u8, usize)>,
 	/// The data generated for the current line
 	current_data: Vec<u8>,
 	/// The adress of the first token of the line
-	current_first_address: u32
+	current_first_address: u32,
 }	
 
 impl Debug for ListingOutput {
@@ -32,6 +36,7 @@ impl ListingOutput {
 	pub fn new<W:'static + Write>(writer: W) -> Self {
 		Self {
 			writer: Box::new(writer),
+			current_source: None,
 			current_line: None,
 			current_data: Vec::new(),
 			current_first_address: 0
@@ -43,41 +48,53 @@ impl ListingOutput {
 	fn process_current_line(&mut self) {
 		// rebuild the string
 		let (ptr, len) = self.current_line.take().unwrap();
-		let line_representation = String::from_utf8_lossy(unsafe{std::slice::from_raw_parts(ptr, len)}).to_string();
+		let line_representation = String::from_utf8_lossy(unsafe{std::slice::from_raw_parts(ptr, len)});
+		let mut line_representation = line_representation.split("\n");
 		// TODO include the other lines for macros and so on
 
 		// Split the bytes in several lines if any
-		let data_representation = self.current_data.iter()
+		let mut data_representation = self.current_data.iter()
 											.chunks(self.bytes_per_line())
 											.into_iter()
 											.map(|c| {
 												c.map(|b| format!("{:X}", b))
 												.join(" ")
-											})
-											.collect_vec();
-		self.current_data.clear();
+											}).collect_vec();
+		let mut data_representation = data_representation.iter();
 
 		// draw the first line
 		writeln!(
 			self.writer,
 			"{:04X} {:bytes_width$} {}",
 			self.current_first_address,
-			data_representation.get(0).unwrap_or(&"".to_owned()),
-			line_representation,
+			data_representation.next().unwrap_or(&"".to_owned()),
+			line_representation.next().unwrap(),
 			bytes_width = self.bytes_per_line()*3
 		).unwrap();
 
-		for i in 1..data_representation.len() {
+		loop {
+
+			let current_line = line_representation.next();
+			let current_data = data_representation.next();
+
+			if current_data.is_none() && current_line.is_none() {
+				break;
+			}
+
 			writeln!(
 				self.writer,
-				"{:04X} {:bytes_width$}",
+				"{:04X} {:bytes_width$} {}",
 				self.current_first_address,
-				data_representation.get(i).unwrap(),
+				current_data.unwrap_or(&"".to_owned()),
+				current_line.unwrap_or(""),
 				bytes_width = self.bytes_per_line()*3
 			).unwrap();
 		
 
 		}
+
+		self.current_data.clear();
+
 	}
 
 	fn bytes_per_line(&self) -> usize {
@@ -86,13 +103,22 @@ impl ListingOutput {
 
 	/// Add a token for the current line
 	pub fn add_token(&mut self, token: &LocatedToken, bytes: &[u8], address: u32) {
+		// pointer slice on the line
 		let token_line = token.span().get_line_beginning();
-		let token_line_size = token_line.len();
+		let token_line_len = token_line.len();
 		let token_line = token_line.as_ptr();
+		let token_line_desc = (token_line, token_line_len);
+
+		// pointer slice on the code
+		let source = &token.span().extra.0.as_str();
+		let source_len = source.len();
+		let source = source.as_ptr();
+		let source_desc = (source, source_len);
 
 		if self.current_line.is_none() {
 			// first call, we add the info
-			self.current_line = Some((token_line, token_line_size));
+			self.current_source = Some(source_desc);
+			self.current_line = Some(token_line_desc);
 			self.current_data.extend_from_slice(bytes);
 			self.current_first_address = address;
 		}
@@ -104,14 +130,31 @@ impl ListingOutput {
 				self.current_data.extend_from_slice(bytes);
 			}
 			else {
-				// new line to handle
+				// enlarge if needed the first line
+				if source_desc == *self.current_source.as_ref().unwrap() {
+					// update the texte length to allow multilines directives to be properly handled
+					let new_line_length = unsafe{token_line_desc.0.offset_from( self.current_line.as_ref().unwrap().0)};
+
+
+					self.current_line.as_mut().unwrap().1 = new_line_length as _;
+
+
+				}
+
+
+				// display previous
 				self.process_current_line();
+
+				// add new token
 				self.add_token(token, bytes, address); // avoid copy paste of similar code
 			}
 		}
 	}
 
 	pub fn finish(&mut self) {
+		// ensure we display the end of the file
+		let new_line_length = unsafe{self.current_source.as_ref().unwrap().1 - (self.current_source.as_ref().unwrap().0.offset_from(self.current_line.as_ref().unwrap().0).abs() as usize)};
+		self.current_line.as_mut().unwrap().1 = new_line_length as _;
 		self.process_current_line()
 	}
 
