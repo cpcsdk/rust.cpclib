3f5d8dce377f835a1f60f929ad375e58d2ff0e29
Romain Giot <romain.giot@u-bordeaux.fr>
2021-07-30 14:59:40 +0200

    Add support for symbols output

---
+++ cpclib-asm/src/assembler/symbols_output.rs
@@ -0,0 +1,38 @@
+use std::io::Write;
+
+use cpclib_tokens::symbols::{SymbolsTableTrait, Symbol};
+
+/// Manage the generation of the symbols output.
+/// Could be parametrize by some directives
+#[derive(Default)]
+pub struct SymbolOutputGenerator {
+
+}
+
+
+impl SymbolOutputGenerator {
+
+	/// Generate the symbol table in w
+	pub fn generate<W: Write>(&self, w: &mut W, symbs: & impl SymbolsTableTrait) -> std::io::Result<()> {
+
+		for &k in symbs.integer_symbols()
+					.iter()
+					.filter(|s| self.keep_symbol(s)) {
+			
+			// TODO add some filtering stuffs based on the directives
+			writeln!(
+				w,
+				"{} equ 0x{:04X}",
+				k.value(),
+				symbs.int_value(k.clone()).unwrap()
+			)?;
+		}
+
+		Ok(())
+	}
+
+	/// Returns true if the symbol needs to be printed
+	pub fn keep_symbol(&self, sym: &Symbol) -> bool {
+		sym.value() != "$"
+	}
+}
