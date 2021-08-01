b4a78889c7209f0b95172124fae5063706f7fdf4
Romain Giot <romain.giot@u-bordeaux.fr>
2021-08-01 09:05:13 +0200

    basm: add ds synonym to defs

--- cpclib-asm/src/parser/parser.rs
+++ cpclib-asm/src/parser/parser.rs
@@ -2027,7 +2027,7 @@ pub fn parse_org(input: Z80Span) -> IResult<Z80Span, Token, VerboseError<Z80Span
 
 /// Parse defs instruction. TODO add optional parameters
 pub fn parse_defs(input: Z80Span) -> IResult<Z80Span, Token, VerboseError<Z80Span>> {
-    let (input, _) = tuple((alt(( parse_instr("FILL"), parse_instr("DEFS"))), space1))(input)?;
+    let (input, _) = tuple((alt(( parse_instr("FILL"), parse_instr("DS"), parse_instr("DEFS"))), space1))(input)?;
 
     let (input, val) = expr(input)?;
 
