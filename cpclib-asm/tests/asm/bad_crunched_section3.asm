be73e17745fa2664489e4813a3bd0b18a047f209
Romain Giot <romain.giot@u-bordeaux.fr>
2021-08-12 10:47:22 +0200

    add additional cases

---
+++ cpclib-asm/tests/asm/bad_crunched_section3.asm
@@ -0,0 +1,12 @@
+; limits and protected are deactivated inside crunch section as we consider they have to be applied to the crunched version
+; however, it is possible to duplicate it in the crunch section (and its life corresponds to the crosssection one only)
+
+
+	org 0x100
+
+	limit 0x1e0
+
+	LZAPU
+	limit 0x1e0
+	defs 0x100
+	LZCLOSE
---
+++ cpclib-asm/tests/asm/good_crunched_section3.asm
@@ -0,0 +1,11 @@
+; limits and protected are deactivated inside crunch section as we consider they have to be applied to the crunched version
+
+
+	org 0x100
+
+	limit 0x1e0
+
+	LZAPU
+	defs 0x100
+	assert $> 0x1e0
+	LZCLOSE
