f21d0f50a5fd4665b7b5a2a0ae958ce66f066451
Romain Giot <romain.giot@u-bordeaux.fr>
2021-09-04 19:36:32 +0200

    Add visual basm

---
+++ visual-basm/src/main.rs
@@ -0,0 +1,10 @@
+mod basm_app;
+use basm_app::*;
+
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    let app = BasmApp::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
+}
