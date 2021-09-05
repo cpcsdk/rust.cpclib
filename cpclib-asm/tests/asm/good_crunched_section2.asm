23cd2f98bc5b4bc63666a69eebc905d98db57e80
Romain Giot <romain.giot@u-bordeaux.fr>
2021-09-05 16:12:24 +0200

    basm: some fixes in the test files

--- cpclib-asm/tests/asm/good_crunched_section2.asm
+++ cpclib-asm/tests/asm/good_crunched_section2.asm
@@ -1,24 +1,11 @@
+; all expressions in crunched section must be resolved directly without the need of an additional pass
 
-	; Illustration of nested crunch section
+
 	org 0x100
 
-CS_START
 
 	LZAPU
-	LZ49
-	repeat 3
-		LZ49
-@INNER_START
-			defs 100
-@INNER_STOP
-			assert @INNER_STOP - @INNER_START == 100
-
-		LZCLOSE
-	endr
-	LZCLOSE
-	db 3
+	db VAR ; fail because VAR is unknown
 	LZCLOSE
 
-CS_STOP
-
-	assert CS_STOP - CS_START < 100*3
+VAR=1
