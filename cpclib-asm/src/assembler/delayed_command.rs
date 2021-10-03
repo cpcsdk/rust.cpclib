cc49fb568f9a06917079c50f532356c5ef672538
Romain Giot <romain.giot@u-bordeaux.fr>
2021-10-03 13:31:48 +0200

    cpclib-asm: reimpliment assert/print/save on the concept of delayed commands (will allow to use a number of pass != 2)

---
+++ cpclib-asm/src/assembler/delayed_command.rs
@@ -0,0 +1,132 @@
+use std::io::Write;
+
+use crate::{error::AssemblerError, preamble::Z80Span};
+use cpclib_common::rayon::prelude::*;
+use super::{Env, report::SavedFile, save_command::SaveCommand};
+use cpclib_common::itertools::Itertools;
+trait DelayedCommand {
+
+}
+
+#[derive(Debug, Clone)]
+pub struct PrintCommand {
+	pub(crate) span: Option<Z80Span>,
+	pub(crate) print_or_error: either::Either<String, AssemblerError>
+}
+#[derive(Debug, Clone)]
+pub struct FailedAssertCommand {
+	failure: AssemblerError
+}
+
+
+/// Expect an assert error or a exval error
+impl From<AssemblerError> for FailedAssertCommand {
+	fn from(failure: AssemblerError) -> Self {
+		Self{failure}
+	}
+}
+
+impl DelayedCommand for PrintCommand {
+
+}
+
+impl DelayedCommand for FailedAssertCommand {
+	
+}
+
+impl PrintCommand {
+	pub fn execute(&self, writer: &mut impl Write) -> Result<(), AssemblerError> {
+		match &self.print_or_error {
+			either::Either::Left(msg) => {
+				// TODO improve printting + integrate z80span information
+				writeln!(writer, "{}", msg);
+				Ok(())
+			},
+			either::Either::Right(e) => Err(e.clone()),
+		}
+	}
+}
+
+#[derive(Debug, Clone)]
+pub struct DelayedCommands {
+	failed_assert_commands: Vec<FailedAssertCommand>,
+	save_commands: Vec<SaveCommand>,
+	print_commands: Vec<PrintCommand>
+}
+
+impl Default for DelayedCommands {
+	fn default() -> Self {
+		Self {
+			failed_assert_commands: Vec::new(),
+			save_commands: Vec::new(),
+			print_commands: Vec::new(),
+		}
+	}
+}
+
+impl DelayedCommands {
+	pub fn clear(&mut self) {
+		self.failed_assert_commands.clear();
+		self.save_commands.clear();
+		self.print_commands.clear();
+	}
+}
+
+/// Commands addition
+impl DelayedCommands {
+	pub fn add_save_command(&mut self, command: SaveCommand) {
+        self.save_commands.push(command);
+    }
+
+	pub fn add_failed_assert_command(&mut self, command: FailedAssertCommand) {
+        self.failed_assert_commands.push(command);
+    }
+
+	pub fn add_print_command(&mut self, command: PrintCommand) {
+        self.print_commands.push(command);
+    }
+}
+
+/// Commands execution
+impl DelayedCommands {
+
+	pub fn execute_save(&self, env: &Env) -> Result<Vec<SavedFile>, AssemblerError> {
+        let res = self
+            .save_commands
+            .par_iter()
+            .map(|cmd| cmd.execute_on(env))
+            .collect::<Result<Vec<_>, AssemblerError>>()?;
+
+        Ok(res)
+    }
+
+	pub fn collect_assert_failure(&self) -> Result<(), AssemblerError> {
+		if self.failed_assert_commands.is_empty() {
+			Ok(())
+		} else {
+			Err(AssemblerError::MultipleErrors{
+				errors: self.failed_assert_commands
+						.iter()
+						.map(|a| a.failure.clone())
+						.collect_vec()
+			})
+		}
+	}
+
+	pub fn execute_print(&self, writer: &mut impl Write) -> Result<(), AssemblerError> {
+		let res = self.print_commands.iter()
+						.map(|p| p.execute( writer))
+						.filter(|r| r.is_err())
+						.map(|e| e.err().unwrap())
+						.collect_vec();
+		if res.is_empty() {
+			 Ok(())
+		}
+		else {
+			 Err(AssemblerError::MultipleErrors {
+				errors: res
+			})
+		}
+	}
+}
+
