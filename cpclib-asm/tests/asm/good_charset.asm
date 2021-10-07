001657f13a5185d1a16b6ea5090f189130f0a97d
Romain Giot <romain.giot@u-bordeaux.fr>
2021-10-07 20:18:43 +0200

    cpclib-asm: add charset example file

---
+++ cpclib-asm/tests/asm/good_charset.asm
@@ -0,0 +1,13 @@
+ org 0x100
+
+ charset "abcdefghijklmnopqrstuvwxyz", 0
+ charset "AB", 100
+ db "aA"
+ ASSERT memory(0x100) == 0x00
+ ASSERT memory(0x101) == 100
+
+ org 0x200
+ charset
+ db "aA"
+ ASSERT memory(0x200) == 'a'
+ ASSERT memory(0x201) == 'A'
