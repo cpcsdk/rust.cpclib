e7256de686a129bb22847c84b8ee3b006fe167b4
Romain Giot <romain.giot@u-bordeaux.fr>
2021-11-04 10:19:19 +0100

    basm: fix some regression issues due to removal of '..' for string delimitation

--- basm/tests/asm/good_delimited.asm
+++ basm/tests/asm/good_delimited.asm
@@ -3,8 +3,8 @@
     db 10 : db 20 : db 30
 test db 40
 
-    defb 'hello: world'
-    defb 'hello' : defb 'world'
+    defb "hello: world"
+    defb "hello" : defb "world"
 
     db 0 ; fdefd : ccds
     db 0 ; 'fdefd :' ccds
