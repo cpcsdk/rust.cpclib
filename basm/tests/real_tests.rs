d775be1f03fbbe90f4e70981ad9e1098279b83b0
Romain Giot <romain.giot@u-bordeaux.fr>
2022-03-17 20:23:41 +0100

    Also add a test to generate symbols

--- basm/tests/real_tests.rs
+++ basm/tests/real_tests.rs
@@ -66,13 +66,48 @@ fn expect_listing_success(fname: &str) {
     let listing_fname = listing_file.path().as_os_str().to_str().unwrap();
 
 
-    println!("Lisitng will be generated in {}", &listing_fname);
     let res = Command::new("../target/debug/basm")
         .args(["-I", "tests/asm/", 
         "-i", 
         fname, 
         "-o", output_fname,
         "--lst", listing_fname
+        ])
+        .output()
+        .expect("Unable to launch basm");
+
+    if !res.status.success() {
+        panic!(
+            "Failure to assemble {}.\n{}",
+            fname,
+            String::from_utf8_lossy(&res.stderr)
+        );
+    }
+
+
+}
+
+
+
+#[test_resources("basm/tests/asm/good_*.asm")]
+/// TODO write tests specifics for this purpose
+fn expect_symbols_success(fname: &str) {
+    let fname = &fname["basm/tests/asm/".len()..];
+
+    let output_file = tempfile::NamedTempFile::new().expect("Unable to build temporary file");
+    let output_fname = output_file.path().as_os_str().to_str().unwrap();
+
+
+    let symbol_file = tempfile::NamedTempFile::new().expect("Unable to build temporary file");
+    let symbol_fname = symbol_file.path().as_os_str().to_str().unwrap();
+
+
+    let res = Command::new("../target/debug/basm")
+        .args(["-I", "tests/asm/", 
+        "-i", 
+        fname, 
+        "-o", output_fname,
+        "--sym", symbol_fname
         ])
         .output()
         .expect("Unable to launch basm");
