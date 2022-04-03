dd8f8d255c6e2b5ac1eb53ef16fbae4486035b88
Romain Giot <romain.giot@u-bordeaux.fr>
2022-04-03 12:38:12 +0200

    basm: add two additional tests

---
+++ basm/tests/asm/good_bit.asm
@@ -0,0 +1,12 @@
+bit 6,(ix+#12)
+bit 7,(ix+#12)
+; these instructions are not valid
+;bit 0,(ix+#12),d
+;bit 1,(ix+#12),b
+;bit 2,(ix+#12),c
+;bit 3,(ix+#12),d
+                         
+;bit 4,(ix+#12),e
+;bit 5,(ix+#12),h
+
+bit 6,(ix+#12)
---
+++ basm/tests/asm/good_ld.asm
@@ -0,0 +1,7 @@
+	ld bc, 1234
+	ld bc, 0x1234
+	ld bc, #1234
+	:ld bc, #1234
+	::ld bc, #1234
+	nop::ld bc, #1234
+
