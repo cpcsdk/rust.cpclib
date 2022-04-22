498458d3ae27f8ac0a224279bdc9d0b0cf104aa4
Romain Giot <romain.giot@u-bordeaux.fr>
2022-04-22 22:18:56 +0200

    cpclib-basic: add some missing tests

---
+++ cpclib-basic/tests/failures.rs
@@ -0,0 +1,17 @@
+use cpclib_basic::BasicProgram;
+
+#[test]
+pub fn failures() {
+	let codes = [
+		"10 PRINT \"HELLO", // Unclosed string
+	];
+
+	for code in codes {
+		let basic = BasicProgram::parse(code);
+		if let Err(err) = basic {
+			// everything is ok
+		} else {
+			panic!("{} has been parsed", code);
+		}
+	}
+}
