48d7d8bdb8df02d43ceb9b43e0ab59ea37b39c92
Romain Giot <romain.giot@u-bordeaux.fr>
2023-03-20 23:00:26 +0100

    [untested] Use claui instead of klask fo visual basm

--- cpclib-visual-basm/src/main.rs
+++ cpclib-visual-basm/src/main.rs
@@ -1,10 +1,9 @@
 use cpclib_asm::basm_utils::*;
-use klask::Settings;
 
 /// #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     let app = build_args_parser();
-    klask::run_app(app, Settings::default(), |matches| {
+    claui::run(app, |matches| {
         dbg!(matches);
 
         match process(matches) {
