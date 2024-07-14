dbe2487b17f2fdaa24fae24f1fc1d64399a16ce6
Krusty/Benediction <krusty.benediction@gmail.com>
2024-07-14 17:21:47 +0200

    [bndbuild] Update the jinja example to use conditionnal build

--- cpclib-bndbuild/tests/jinja/test.asm
+++ cpclib-bndbuild/tests/jinja/test.asm
@@ -1,2 +1,22 @@
-	org 0x100
-	jp $
+	org 0x1000
+	run $
+beginning
+	
+	ld hl, data
+loop
+	ld a, (hl)
+	or a : jp z, $
+	call 0xbb5a
+	inc hl
+	jp loop
+
+data
+	db "HELLO WORLD FROM "
+	ifdef BASM
+	 db "basm"
+	else
+	 db "rasm"
+	endif
+	db 0
+
+	save "TEST", beginning, $-beginning, AMSDOS
