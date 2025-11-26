f4cc37457a6ab200ea85a55a513c73c1723284b1
Krusty/Benediction <krusty.benediction@gmail.com>
2025-11-26 11:04:14 +0100

    Apply cargo clippy --fix

--- cpclib-imgconverter/src/bin/cpc2img.rs
+++ cpclib-imgconverter/src/bin/cpc2img.rs
@@ -1,7 +1,4 @@
-use cpclib::common::clap::{self, Arg, ArgAction, Command, value_parser};
-use cpclib::disc::amsdos::AmsdosError;
-use cpclib::image::image::{ColorMatrix, Mode};
-use cpclib_imgconverter::{self, build_cpc2img_args_parser, get_requested_palette, specify_palette};
+use cpclib_imgconverter::{self, build_cpc2img_args_parser};
 
 fn main() -> Result<(), String> {
     let cmd = build_cpc2img_args_parser();
