88a89662771e581d2a41ef081ac9fe38b66fe6ab
Krusty/Benediction <krusty.benediction@gmail.com>
2025-12-28 15:13:36 +0100

    [bndbuild] temporarily add some debug print. to be removed later when the feature will be finalized.

--- cpclib-bndbuild/src/builder.rs
+++ cpclib-bndbuild/src/builder.rs
@@ -314,6 +314,13 @@ impl BndBuilder {
             }
         }
 
+
+        dbg!(
+            parallel_tasks.len(),
+            serial_tasks.len(),
+            without_rule.len()
+        );
+
         // count the files that are not produced
         for targets in without_rule.into_iter() {
             #[cfg(feature = "rayon")]
