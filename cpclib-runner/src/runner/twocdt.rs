f02c77a8b13369b58cd3d73ed63a0dd0d43517f1
Krusty/Benediction <krusty.benediction@gmail.com>
2026-03-22 22:05:55 +0100

    [bndbuild] Fix 2cdt/linux call

--- cpclib-runner/src/runner/twocdt.rs
+++ cpclib-runner/src/runner/twocdt.rs
@@ -28,26 +28,11 @@ impl TwoCdtVersion {
         #[cfg(not(target_os = "windows"))]
         let exec_fname: &'static str = "2cdt";
 
-        #[cfg(target_os = "linux")]
-        let compile: Option<Compiler<E>> = {
-            let install: Box<dyn Fn(&Utf8Path, &E) -> Result<(), String>> =
-                Box::new(|_path: &Utf8Path, o: &E| -> Result<(), String> {
-                    ExternRunner::default().inner_run(
-                        &["gcc", "2cdt.c", "-O2", "-o", "2cdt"],
-                        o
-                    )
-                });
-            Some(Compiler::from(install))
-        };
-        #[cfg(not(target_os = "linux"))]
-        let compile: Option<Compiler<E>> = None;
-
         DelegateApplicationDescription::builder()
             .download_fn_url("https://cpctech.cpcwiki.de/download/2cdt.zip")
             .folder("2cdt")
             .archive_format(ArchiveFormat::Zip)
             .exec_fname(exec_fname)
-            .maybe_compile(compile)
             .build()
     }
 }
