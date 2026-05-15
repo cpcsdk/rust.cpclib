acff13d8e2c4f2f5aa83df59b36a317d3531005b
Krusty/Benediction <krusty.benediction@gmail.com>
2026-05-15 11:34:39 +0200

    fix martine isntaller for macosx

--- cpclib-runner/src/runner/martine.rs
+++ cpclib-runner/src/runner/martine.rs
@@ -80,6 +80,9 @@ cfg_select! {
                                     "binaries/martine-darwin-amd64/martine.app/Contents/MacOS/martine"
                                 );
                                 let wrapper = desc.exec_fname();
+                                if let Some(parent) = wrapper.parent() {
+                                    fs_err::create_dir_all(parent).map_err(|e| e.to_string())?;
+                                }
 
                                 // Ensure the downloaded binary remains executable.
                                 let mut real_perms = fs_err::metadata(&real_exec)
@@ -124,12 +127,16 @@ exec "$MARTINE_EXEC" "$@"
                             .post_install(post_install)
                             .build()
                     },
-                    MartineVersion::V0_39 => DelegateApplicationDescription::builder()
-                        .download_fn_url("https://github.com/jeromelesaux/martine/releases/download/v0.39/martine-0.39-darwin-amd64.zip")
-                        .folder("martine_0_39")
-                        .archive_format(ArchiveFormat::Zip)
-                        .exec_fname("martine.app/Contents/MacOS/martine")
-                        .build(),
+                    MartineVersion::V0_39 => {
+   
+
+                        DelegateApplicationDescription::builder()
+                            .download_fn_url("https://github.com/jeromelesaux/martine/releases/download/v0.39/martine-0.39-darwin-amd64.zip")
+                            .folder("martine_0_39")
+                            .archive_format(ArchiveFormat::Zip)
+                            .exec_fname("martine-darwin-amd64/martine.app/Contents/MacOS/cli")
+                            .build()
+                    },
                 }
             }
         }
